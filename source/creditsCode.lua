d90d80c02cd9bb51db30cbc1b914e580f8d4c392
AquaStrikr <69995597+AquaStrikr@users.noreply.github.com>
2022-07-29 01:35:56 -0300

    PINTOOOOO

---
+++ source/creditsCode.lua
@@ -0,0 +1,4 @@
+--Credits
+AquaStrikr = "Owner, Programmer and Creator"
+Mibsfiha = "Additional Programmer and Brick Eater"
+Weedgee = "he uses this shit everyday"
---
+++ source/tylenol5MAINState.lua
@@ -0,0 +1,19 @@
+--tylenol5 created and coded by AquaStrikr
+
+        variavel = tylenol 
+    tylenol5 = true
+
+              turnsIntoLuigifromMario64 = dies [false]
+
+    tylenol.mid = false
+       getDrogado = true
+    bitcheless = nil
+
+         User.action = usar-o-tylenol5
+            User.consequence = ficar-drogado
+    User.aftermath = unknown
+
+        User = turnsIntoLuigifromMario64 == dies [true]
+    tylenol5.overdose = true
+            only = turnsIntoLuigifromMario64
+
